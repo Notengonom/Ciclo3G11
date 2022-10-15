f540b810675e13784d3828debd5bf30247d45686
jaiberj <jaiberj1@gmail.com>
2022-10-15 12:46:15 -0500

    commit 2

--- src/main/java/com/fredypuentes/Ciclo3G11/Ciclo3G11Application.java
+++ src/main/java/com/fredypuentes/Ciclo3G11/Ciclo3G11Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("Model")
-@EnableJpaRepositories("Repository")
 public class Ciclo3G11Application {
 
 	public static void main(String[] args) {
